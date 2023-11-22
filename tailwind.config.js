6fa86c6469186344e4e459a8412da7c9f29fe9c3
Raymel Francisco <franciscoraymel@gmail.com>
2023-11-23 05:52:02 +0800

    main: Initial dark mode setup with DaisyUI and TailwindCSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  darkMode: 'class', // This can be 'media' if you want to use the OS level setting
   theme: {
     extend: {
       colors: {
         "cust-indigo-dark": "#3D348B",
         "cust-indigo-light": "#7678ED"
-      }
+      },
+
     },
   },
   plugins: [require('@tailwindcss/typography'), require("daisyui")],
   daisyui: {
     themes: [
+      "cupcake",
       {
-        mytheme: {
+        light: {
           "primary": "#DE5745",
           "secondary": "#E5C12E",
           "accent": "#264E6E",
@@ -25,7 +28,44 @@ module.exports = {
           "warning": "#fbbd23",
           "error": "#f87272"
         },
+        dark: {
+          "primary": "#DE5745",
+          "secondary": "#E5C12E",
+          "accent": "#264E6E",
+          "neutral": "#2a323c",
+          "base-100": "#1d232a",
+          "info": "#3abff8",
+          "success": "#36d399",
+          "warning": "#fbbd23",
+          "error": "#f87272"
+        },
       },
     ]
+    // themes: [
+    //   {
+    //     light: {
+    //       "primary": "#DE5745",
+    //       "secondary": "#E5C12E",
+    //       "accent": "#264E6E",
+    //       "neutral": "#2a323c",
+    //       "base-100": "#ffffff",
+    //       "info": "#3abff8",
+    //       "success": "#36d399",
+    //       "warning": "#fbbd23",
+    //       "error": "#f87272"
+    //     },
+    //     dark: {
+    //       "primary": "#DE5745",
+    //       "secondary": "#E5C12E",
+    //       "accent": "#264E6E",
+    //       "neutral": "#2a323c",
+    //       "base-100": "#1d232a",
+    //       "info": "#3abff8",
+    //       "success": "#36d399",
+    //       "warning": "#fbbd23",
+    //       "error": "#f87272"
+    //     },
+    //   }
+    // ]
   }
 }
