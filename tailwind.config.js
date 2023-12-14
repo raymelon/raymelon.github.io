4817e1d1784060c5098e77a5340768afb087374f
Raymel Francisco <franciscoraymel@gmail.com>
2023-12-15 02:08:04 +0800

    main: Fix tile background that is too light on light mode, along with dark and light mode theming improvements

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,12 +3,17 @@ module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   darkMode: 'class', // This can be 'media' if you want to use the OS level setting
   theme: {
+    // tailwind theme extension
+    // this is different from daisyUI theme
+    // use with caution
     extend: {
       colors: {
         "cust-indigo-dark": "#3D348B",
         "cust-indigo-light": "#7678ED"
       },
-
+      // backgroundColor: theme => ({
+      //   "cust-background-color": "rgb(250, 235, 215, 0.1)"
+      // })
     },
   },
   plugins: [require('@tailwindcss/typography'), require("daisyui")],
@@ -45,56 +50,13 @@ module.exports = {
       "nord",
       "sunset",
       {
-        light: {
-          "primary": "#DE5745",
-          "secondary": "#E5C12E",
-          "accent": "#264E6E",
-          "neutral": "#2a323c",
-          // "base-100": "#1d232a",
-          "base-100": "#ffffff",
-          "info": "#3abff8",
-          "success": "#36d399",
-          "warning": "#fbbd23",
-          "error": "#f87272"
-        },
         dark: {
-          "primary": "#DE5745",
-          "secondary": "#E5C12E",
-          "accent": "#264E6E",
-          "neutral": "#2a323c",
-          "base-100": "#1d232a",
-          "info": "#3abff8",
-          "success": "#36d399",
-          "warning": "#fbbd23",
-          "error": "#f87272"
+          ...require("daisyui/src/theming/themes")["[data-theme=dracula]"],
+        },
+        light: {
+          ...require("daisyui/src/theming/themes")["[data-theme=lemonade]"],
         },
       },
     ]
-    // themes: [
-    //   {
-    //     light: {
-    //       "primary": "#DE5745",
-    //       "secondary": "#E5C12E",
-    //       "accent": "#264E6E",
-    //       "neutral": "#2a323c",
-    //       "base-100": "#ffffff",
-    //       "info": "#3abff8",
-    //       "success": "#36d399",
-    //       "warning": "#fbbd23",
-    //       "error": "#f87272"
-    //     },
-    //     dark: {
-    //       "primary": "#DE5745",
-    //       "secondary": "#E5C12E",
-    //       "accent": "#264E6E",
-    //       "neutral": "#2a323c",
-    //       "base-100": "#1d232a",
-    //       "info": "#3abff8",
-    //       "success": "#36d399",
-    //       "warning": "#fbbd23",
-    //       "error": "#f87272"
-    //     },
-    //   }
-    // ]
   }
 }
